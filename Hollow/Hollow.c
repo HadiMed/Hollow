a643c87dfe4f9551c77bb94f2d15517e2ecb8bc9
Foxnight-bit <57273771+Foxnight-bit@users.noreply.github.com>
2021-12-04 00:56:40 +0100

    Kernel32 , ntdll bases okey

--- Hollow/Hollow.c
+++ Hollow/Hollow.c
@@ -1,12 +1,24 @@
 // 
 #include <windows.h>
+#include <stdio.h>
 
-DWORD kernel32base()
+
+DWORD kernel32_base, ntdll_base; 
+
+
+void DLL_bases()
 {
 	__asm {
 		xor eax, eax
 		mov eax, fs:[eax + 0x30] //PEB
-		mov eax , [eax+0xc] // Load
+		mov eax , [eax+0xc] // pointer LDR 
+		mov eax , [eax+0x14] // InMemoryOrderModuleList
+		mov eax, [eax]	// image base of ntdll
+		mov ecx , [eax+0x10]
+		mov [ntdll_base] , ecx
+		mov eax , [eax] // kernel32.dll
+		mov ecx , [eax+0x10]
+		mov [kernel32_base] , ecx
 	}
 
 }
@@ -15,5 +27,8 @@ DWORD kernel32base()
 
 int wmain()
 {
-	return 0;
+	DLL_bases(); 
+
+	printf("ntdll base : %x\nkernel32_Base : %x\n", ntdll_base ,kernel32_base);
+
 }
