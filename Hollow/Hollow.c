48c9b2457c477a1ec19316fa433eb2faa05b848e
Foxnight-bit <57273771+Foxnight-bit@users.noreply.github.com>
2021-12-04 01:49:08 +0100

    name function

--- Hollow/Hollow.c
+++ Hollow/Hollow.c
@@ -6,19 +6,19 @@
 DWORD kernel32_base, ntdll_base; 
 
 
-void DLL_bases()
+void Kernel32_NTdll_bases()
 {
 	__asm {
 		xor eax, eax
 		mov eax, fs:[eax + 0x30] //PEB
 		mov eax , [eax+0xc] // pointer LDR 
 		mov eax , [eax+0x14] // InMemoryOrderModuleList
-		mov eax, [eax]	// image base of ntdll
-		mov ecx , [eax+0x10]
+		mov eax, [eax]	 
+		mov ecx , [eax+0x10] // image base of ntdll 
 		mov [ntdll_base] , ecx
 		mov eax , [eax] // kernel32.dll
 		mov ecx , [eax+0x10]
-		mov [kernel32_base] , ecx
+		mov [kernel32_base] , ecx // image base of kernel32.dll
 	}
 
 }
@@ -27,8 +27,7 @@ void DLL_bases()
 
 int wmain()
 {
-	DLL_bases(); 
-
+	Kernel32_NTdll_bases(); 
 	printf("ntdll base : %x\nkernel32_Base : %x\n", ntdll_base ,kernel32_base);
 
 }
